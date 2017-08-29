1410bd4abf2a858ad1276dcef90d798221ec4594
dshovchko <d.shovchko@gmail.com>
2017-08-29 22:13:22 +0300

    add autoloader

---
+++ autoloader.php
@@ -0,0 +1,21 @@
+<?php
+
+function __autoload_ottTrinity($class_name) 
+{
+        if (substr($class_name, 0, 11) != 'ottTrinity_')
+        {
+                return FALSE;
+        }
+        $filename = str_replace('_', DIRECTORY_SEPARATOR, substr($class_name, 11)).'.php';
+
+        $file = dirname(__FILE__).'/class/'.$filename;
+
+        if ( ! file_exists($file))
+        {
+                return FALSE;
+        }
+        require $file;
+}
+
+spl_autoload_extensions('.php');
+spl_autoload_register('__autoload_ottTrinity');
